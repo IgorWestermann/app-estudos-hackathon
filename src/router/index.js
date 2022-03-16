9dd694b3e4b645a4d1dab0264824cc1517d00bce
IgorWestermann <i.westma@gmail.com>
2022-03-16 17:09:56 -0300

    add favorites page and routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomePage from '../views/HomePage.vue'
+import FavoritesPage from '../views/FavoritesPage.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'home',
     component: HomePage
+  },
+  {
+    path: '/',
+    name: 'Favorites',
+    component: FavoritesPage
   },
 ]
 
---
+++ src/views/FavoritesPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <h2>Favorites</h2>
+</template>
+
+<script>
+export default {
+  name: "Favorites",
+}
+</script>
+
+<style>
+
+</style>
